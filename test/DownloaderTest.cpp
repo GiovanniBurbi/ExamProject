828eb9f95688efdfa97a4f251b59faf9786d8829
giovanni <giovanniburbi10@gmail.com>
2016-09-20 19:43:51 +0200

    fourth commit. Aggiunta gui e sistemato test e main, più lettura file implementata

--- test/DownloaderTest.cpp
+++ test/DownloaderTest.cpp
@@ -3,20 +3,41 @@
 //
 #include <gtest/gtest.h>
 #include "../Downloader.h"
+#include <cstdio>
 
 
 TEST(Downloader, TestaddFiles){
     Downloader dload;
-    dload.addFile("testfile");
-    dload.addFile("testfile2");
+    FILE* file1;
+    file1=fopen("/home/giovanni/ClionProjects/Progetto/File1.txt","r");
+    dload.addFile(file1);
+    FILE* file2;
+    file2=fopen("/home/giovanni/ClionProjects/Progetto/File1.txt","r");
+    dload.addFile(file2);
     ASSERT_EQ(2, dload.getNumFiles());
 }
 
 TEST(Downloader, TestDownload){
     Downloader dload;
-    dload.addFile("testfile");
+    FILE* file1;
+    file1=fopen("/home/giovanni/ClionProjects/Progetto/File1.txt","r");
+    dload.addFile(file1);
+    FILE* file2;
+    file2=fopen("/home/giovanni/ClionProjects/Progetto/File1.txt","r");
+    dload.addFile(file2);
     dload.downloadFiles();
-    ASSERT_EQ(0, dload.getNumFiles());
+    ASSERT_EQ(0,dload.getNumFiles());
 
 }
 
+TEST(Downloader, TestGetByte){
+    Downloader dload;
+    FILE* file1;
+    file1=fopen("/home/giovanni/ClionProjects/Progetto/File1.txt","r");
+    dload.addFile(file1);
+    FILE* file2;
+    file2=fopen("/home/giovanni/ClionProjects/Progetto/File2.txt","r");
+    dload.addFile(file2);
+    ASSERT_EQ(194,dload.getTotalBytes());
+
+}
